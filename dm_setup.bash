40a71d3cfef4f205d8d5bc0209c34333ea0c5ba6
Jeong Han Lee <jeonghan.lee@gmail.com>
2016-09-08 23:44:04 +0200

    add GPL2

--- dm_setup.bash
+++ dm_setup.bash
@@ -1,10 +1,26 @@
 #!/bin/bash
-# 
+#
+#  Copyright (c) 2016 Jeong Han Lee
+#  Copyright (c) 2016 European Spallation Source
+#
+#  The dm_setup.bash is free software: you can redistribute
+#  it and/or modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation, either version 2 of the
+#  License, or any newer version.
+#
+#  This program is distributed in the hope that it will be useful, but WITHOUT
+#  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+#  more details.
+#
+#  You should have received a copy of the GNU General Public License along with
+#  this program. If not, see https://www.gnu.org/licenses/gpl-2.0.txt
+#
 # Shell  : dm_setup.bash
 # Author : Jeong Han Lee
 # email  : han.lee@esss.se
 # Date   : 
-# version : 0.1.0 
+# version : 0.9.0 
 #
 # http://www.gnu.org/software/bash/manual/bashref.html#Bash-Builtins
 
